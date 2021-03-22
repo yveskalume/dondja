95ff7fa2d9845f227eda6cfc27a113a952681c76
YvesKalume <yvkalume@gmail.com>
2021-03-22 13:10:59 +0200

    feed tabs and fragments

--- app/src/main/java/com/dondja/dondja/ui/adapter/FeedPagerAdapter.kt
+++ app/src/main/java/com/dondja/dondja/ui/adapter/FeedPagerAdapter.kt
@@ -1,32 +1,16 @@
 package com.dondja.dondja.ui.adapter
 
 
-import android.content.Context
-import android.view.LayoutInflater
-import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView
-import com.airbnb.epoxy.EpoxyRecyclerView
-import com.dondja.dondja.databinding.LayoutFeedRecyclerviewBinding
-import com.dondja.dondja.util.CustomViewHolder
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
 
-class FeedPagerAdapter(private val context: Context) : RecyclerView.Adapter<CustomViewHolder>() {
-    val rv : EpoxyRecyclerView = EpoxyRecyclerView(context)
-
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        val binding = LayoutFeedRecyclerviewBinding.inflate(layoutInflater,parent,false)
-        return CustomViewHolder(binding)
-
-    }
-
-    override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
-        val binding = holder.binding
-    }
-
+class FeedPagerAdapter(activity: FragmentActivity, private val fragments: List<Pair<String, Fragment>>) : FragmentStateAdapter(activity) {
     override fun getItemCount(): Int {
-        return 1
+        return fragments.size
     }
 
-
+    override fun createFragment(position: Int): Fragment {
+        return fragments[position].second
+    }
 }
--- app/src/main/java/com/dondja/dondja/ui/fragment/feed/FeedFragment.kt
+++ app/src/main/java/com/dondja/dondja/ui/fragment/feed/FeedFragment.kt
@@ -1,23 +1,56 @@
 package com.dondja.dondja.ui.fragment.feed
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.viewbinding.library.fragment.viewBinding
 import com.dondja.dondja.R
 import com.dondja.dondja.databinding.FragmentFeedBinding
 import com.dondja.dondja.ui.adapter.FeedPagerAdapter
+import com.dondja.dondja.ui.fragment.feedall.FeedAllFragment
+import com.dondja.dondja.ui.fragment.feedfollowing.FeedFollowingFragment
+import com.dondja.dondja.ui.fragment.feedgroup.FeedGroupFragment
+import com.dondja.dondja.ui.fragment.feedhashtag.FeedHashtagFragment
+import com.google.android.material.tabs.TabLayoutMediator
 
 class FeedFragment : Fragment(R.layout.fragment_feed) {
 
     private val binding by viewBinding<FragmentFeedBinding>()
 
-    fun setUpFeed() {
-        FeedPagerAdapter(requireContext()).rv.withModels {
+    private val viewPager by lazy { binding.viewPager }
+    private val tabLayout by lazy { binding.tabLayout }
 
-        }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setUpPager()
+    }
+
+    private fun setUpPager() {
+        val fragments = listOf(
+            "Tout" to FeedAllFragment(),
+            "Groupes" to FeedGroupFragment(),
+            "Following" to FeedFollowingFragment(),
+            "Hashtag" to FeedHashtagFragment()
+        )
+
+       val feedPagerAdapter = FeedPagerAdapter(requireActivity(),fragments).also {
+           viewPager.adapter = it
+       }
+
+        TabLayoutMediator(tabLayout, viewPager
+        ) { tab, position ->
+
+//            viewPager.currentItem = position
+            tab.text = fragments[position].first
+            viewPager.currentItem = 0
+            if (feedPagerAdapter.itemCount > 0) {
+//                val currentItem = viewPager.currentItem
+//                if (currentItem != tabLayout.selectedTabPosition) {
+//                    tabLayout.getTabAt(currentItem)?.select()
+//                }
+            }
+        }.attach()
     }
 
 }
