357cd4a542b86364fc0817fcd5f19b5880c31209
YvesKalume <yvkalume@gmail.com>
2021-04-10 22:46:30 +0200

    add bottom nav

--- app/src/main/java/com/dondja/dondja/ui/activity/MainActivity.kt
+++ app/src/main/java/com/dondja/dondja/ui/activity/MainActivity.kt
@@ -2,13 +2,29 @@ package com.dondja.dondja.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.ui.setupWithNavController
 import com.airbnb.epoxy.Carousel
 import com.dondja.dondja.R
+import com.dondja.dondja.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val navController by lazy {
+        Navigation.findNavController(this,R.id.fragmentHost)
+    }
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         Carousel.setDefaultGlobalSnapHelperFactory(null)
+        setUpBottomNav()
+    }
+
+    private fun setUpBottomNav() {
+        binding.bottomNavigation.setupWithNavController(navController)
     }
 }
