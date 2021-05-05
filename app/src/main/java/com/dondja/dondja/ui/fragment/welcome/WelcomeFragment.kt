198b709fccff2c8efcc69b5381554c7c719dfa3e
YvesKalume <yvkalume@gmail.com>
2021-05-05 19:17:39 +0200

    welcome screen
    Signed-off-by: YvesKalume <yvkalume@gmail.com>

---
+++ app/src/main/java/com/dondja/dondja/ui/fragment/welcome/WelcomeFragment.kt
@@ -0,0 +1,14 @@
+package com.dondja.dondja.ui.fragment.welcome
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.viewbinding.library.fragment.viewBinding
+import com.dondja.dondja.R
+import com.dondja.dondja.databinding.FragmentWelcomeBinding
+
+class WelcomeFragment : Fragment(R.layout.fragment_welcome) {
+    private val binding by viewBinding<FragmentWelcomeBinding>()
+}
