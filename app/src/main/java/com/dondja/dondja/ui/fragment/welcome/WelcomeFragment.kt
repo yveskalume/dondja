75dc6d63ced58ab117ca11fcc71ebe78df2dd62f
YvesKalume <yvkalume@gmail.com>
2021-05-05 20:54:01 +0200

    signin and welcome screen
    Signed-off-by: YvesKalume <yvkalume@gmail.com>

--- app/src/main/java/com/dondja/dondja/ui/fragment/welcome/WelcomeFragment.kt
+++ app/src/main/java/com/dondja/dondja/ui/fragment/welcome/WelcomeFragment.kt
@@ -6,9 +6,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.viewbinding.library.fragment.viewBinding
+import androidx.navigation.fragment.findNavController
 import com.dondja.dondja.R
 import com.dondja.dondja.databinding.FragmentWelcomeBinding
 
 class WelcomeFragment : Fragment(R.layout.fragment_welcome) {
     private val binding by viewBinding<FragmentWelcomeBinding>()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setUpListener()
+    }
+
+    private fun setUpListener() {
+
+        binding.loginBtn.setOnClickListener {
+            val direction = WelcomeFragmentDirections.toSignInFragment()
+            findNavController().navigate(direction)
+        }
+    }
 }
