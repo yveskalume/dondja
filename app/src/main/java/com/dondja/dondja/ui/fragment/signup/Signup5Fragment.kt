44912fbc2fc030d7f49287b62ec189c15f87df28
YvesKalume <yvkalume@gmail.com>
2021-06-11 20:06:13 +0200

    signup 5 fragment
    Signed-off-by: YvesKalume <yvkalume@gmail.com>

---
+++ app/src/main/java/com/dondja/dondja/ui/fragment/signup/Signup5Fragment.kt
@@ -0,0 +1,12 @@
+package com.dondja.dondja.ui.fragment.signup
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.dondja.dondja.R
+
+class Signup5Fragment : Fragment(R.layout.fragment_signup5) {
+
+}
