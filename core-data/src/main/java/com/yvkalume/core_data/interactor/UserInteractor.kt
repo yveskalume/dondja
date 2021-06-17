dbac58270ccce2bf2a5024c651eb5212117b878f
YvesKalume <yvkalume@gmail.com>
2021-06-17 18:10:40 +0200

    update(interactor) : UserInteractor.kt
    Signed-off-by: YvesKalume <yvkalume@gmail.com>

--- core-data/src/main/java/com/yvkalume/core_data/interactor/UserInteractor.kt
+++ core-data/src/main/java/com/yvkalume/core_data/interactor/UserInteractor.kt
@@ -1,18 +1,29 @@
 package com.yvkalume.core_data.interactor
 
+import com.google.firebase.firestore.FirebaseFirestore
 import com.yvkalume.core_data.entity.User
+import com.yvkalume.core_data.firebaseconstant.FirestoreReferences
 import com.yvkalume.core_data.util.Crud
 import com.yvkalume.core_data.util.Result
+import com.yvkalume.core_data.util.collectAsFlow
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class UserInteractor @Inject constructor(): Crud<User> {
+@ExperimentalCoroutinesApi
+class UserInteractor @Inject constructor(
+    val  firestore: FirebaseFirestore
+): Crud<User> {
+
+    private val ref = FirestoreReferences
     override fun getAll(): Flow<Result<List<User>>> {
-        TODO("Not yet implemented")
+        val query = firestore.collection(ref.users)
+        return query.collectAsFlow()
     }
 
     override fun getOneById(uid: String): Flow<Result<User>> {
-        TODO("Not yet implemented")
+        val query = firestore.document("${ref.users}/$uid")
+        return query.collectAsFlow()
     }
 
     override fun updateOneByUid(uid: String): Flow<Result<Unit>> {
