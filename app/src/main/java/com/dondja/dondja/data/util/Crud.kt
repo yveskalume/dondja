279bf6dd644b430b9c64e316d188e5b16c9b9ee2
YvesKalume <yvkalume@gmail.com>
2021-07-09 10:53:14 +0200

    update crud interface
    Signed-off-by: YvesKalume <yvkalume@gmail.com>

--- app/src/main/java/com/dondja/dondja/data/util/Crud.kt
+++ app/src/main/java/com/dondja/dondja/data/util/Crud.kt
@@ -3,9 +3,9 @@ package com.dondja.dondja.data.util
 import kotlinx.coroutines.flow.Flow
 
 interface Crud<T> {
-    fun getAll() : Flow<Result<List<T>>>
-    fun getOneById(uid: String) : Flow<Result<T>>
-    suspend fun updateOne(data: T)
-    suspend fun deleteOneByUid(uid: String)
-    fun getAllFromFlowing() : Flow<Result<List<T>>>
+    fun getAll() : Flow<Result<List<T>>> { TODO() }
+    fun getOneById(uid: String) : Flow<Result<T>> { TODO() }
+    suspend fun updateOne(data: T) { TODO() }
+    suspend fun deleteOneByUid(uid: String) { TODO() }
+    fun getAllFromFlowing() : Flow<Result<List<T>>> { TODO() }
 }
